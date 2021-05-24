e8dc4fed4725ecac77bfdb252ab2f0a2676b3ec0
André Luan <andreluan176@gmail.com>
2021-05-24 20:13:27 -0300

    Post para livros

--- src/main/java/com/andre/bookstore/service/LivroService.java
+++ src/main/java/com/andre/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.andre.bookstore.domain.Categoria;
 import com.andre.bookstore.domain.Livro;
 import com.andre.bookstore.repositories.LivroRepository;
 import com.andre.bookstore.service.exception.ObjectNotFoundException;
@@ -43,6 +44,13 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 
 }
